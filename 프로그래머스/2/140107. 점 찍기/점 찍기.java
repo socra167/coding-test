fe150d1aa3ef286a38cb8d704a3ffec31e0b94af
Dongwoo Shin <socra167@gmail.com>
2025-02-09 15:22:28 +0900

    [level 2] Title: 점 찍기, Time: 20.72 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        
+        for (int i = 0; i <= d; i += k) {
+            answer += Math.floor((Math.sqrt(Math.pow(d, 2) - Math.pow(i, 2))) / k + 1);
+        }
+        
+        return answer;
+    }
+}
