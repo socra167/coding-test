343189d1a87be02bfc628a2f9610a9fc4d4da238
Dongwoo Shin <socra167@gmail.com>
2025-03-28 22:04:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 89.40 ms, Memory: 97.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> runners = new HashMap<>();
+        
+        for (String runner : participant) {
+            if (runners.keySet().contains(runner)) {
+                runners.put(runner, runners.get(runner) + 1);
+                continue;
+            }
+            runners.put(runner, 0);
+        }
+        
+        for (String completedRunner : completion) {
+            if (runners.keySet().contains(completedRunner)) {
+                if (runners.get(completedRunner) == 0) {
+                    runners.remove(completedRunner);
+                    continue;
+                }
+                runners.put(completedRunner, runners.get(completedRunner) - 1);
+            }
+        }
+        
+        return runners.keySet().stream()
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("완주하지 못한 선수가 없습니다."));
+    }
+}
