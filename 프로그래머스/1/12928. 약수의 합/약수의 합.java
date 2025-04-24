c36cf05523db3e186edce177994201f6f79df828
Dongwoo Shin <socra167@gmail.com>
2025-04-24 12:58:00 +0900

    [level 1] Title: 약수의 합, Time: 1.55 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 1; i <= n; i ++) {
+            if (n % i == 0) {
+                answer.add(i);
+            }
+        }
+        
+        return (int) answer.stream().mapToInt(i -> i).sum();
+    }
+}
