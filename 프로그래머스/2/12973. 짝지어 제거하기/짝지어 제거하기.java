aa5f13098b3bd0118cb158b1c4133fcf95b744f3
Dongwoo Shin <socra167@gmail.com>
2026-01-07 14:18:17 +0900

    [level 2] Title: 짝지어 제거하기, Time: 61.45 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
