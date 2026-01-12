58fbb55a900bfcea4062ebe2894fbcb1220c83ad
Dongwoo Shin <socra167@gmail.com>
2026-01-12 15:13:59 +0900

    [level 2] Title: 카펫, Time: 16.04 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842#) 
 
 ### 성능 요약
 
-메모리: 88.7 MB, 시간: 13.70 ms
+메모리: 79.6 MB, 시간: 16.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 12일 15:09:56
+2026년 01월 12일 15:13:57
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,27 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
-        
-        List<List<Integer>> yellows = new ArrayList<>();
+        List<List<Integer>> yellowFactors = new ArrayList<>();
         
         for (int i = 1; i <= (yellow / 2 + 1); i++) {
             if (yellow % i == 0) {
-                yellows.add(List.of(yellow / i, i));
+                yellowFactors.add(List.of(yellow / i, i));
             }
         }
         
         brown += yellow;
         for (int i = 1; i <= (brown / 2); i++) {
             if (brown % i == 0) {
-                for (List<Integer> yellowa : yellows) {
-                    if ((yellowa.get(0) + 1 < (brown / i)) && (yellowa.get(1) + 1 < i)) {
+                for (List<Integer> yellowFactor : yellowFactors) {
+                    if ((yellowFactor.get(0) + 1 < (brown / i)) && (yellowFactor.get(1) + 1 < i)) {
                         return new int[]{brown / i, i};
                     }
                 }
             }
         }
         
-        return answer;
+        return new int[]{};
     }
 }
